528366801a07912140479ef2c376e4d467417809
mamenDseid <mamelala@gmail.com>
2022-06-15 15:26:24 +0000

    cd

--- mealApp/App.js
+++ mealApp/App.js
@@ -1,25 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz6sb6tYBbDyKY63aaPd7RG6yss-_2J1c",
+  authDomain: "mealstogo-88089.firebaseapp.com",
+  projectId: "mealstogo-88089",
+  storageBucket: "mealstogo-88089.appspot.com",
+  messagingSenderId: "159902997203",
+  appId: "1:159902997203:web:179f40404419ea076bd226",
+  };
+
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-
-//git remote add origin https://github.com/mamenDseid/MealsApp.git
-//git branch -M main
-//git push -u origin main
