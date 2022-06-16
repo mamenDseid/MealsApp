594bb8cb8d56b2f2e6465b146997f506cf0ff3f2
mamenDseid <mamelala@gmail.com>
2022-06-16 14:09:10 +0000

    commit

--- mealApp/src/services/location/location.context.js
+++ mealApp/src/services/location/location.context.js
@@ -23,7 +23,6 @@ export const LocationContextProvider = ({ children }) => {
     locationRequest(keyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
-        setError(null);
         setIsLoading(false);
         setLocation(result);
       })
--- mealApp/src/services/restaurants/restaurants.service.js
+++ mealApp/src/services/restaurants/restaurants.service.js
@@ -1,58 +1,25 @@
+import { mocks } from "./mock";
 import camelize from "camelize";
-import { host, isMock } from "../../utils/env";
-import {mocks} from './mock'
 
-
-
-export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject)=> {
-const mock = mocks[location]
-
-if(!mock){
-  reject('not found')
-}
-
-resolve(mock)
-  })
- 
-};
-
-
-/*
-
-export const restaurantsRequest = (location) => {
-  return fetch(`${host}/placesNearby?location=${location}&mock=${isMock}`).then(
-    (res) => {
-      return res.json();
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("not found");
     }
-  );
-};
-
-
-export const restaurantsRequest = (location) => {
-  return new Promise((resolve, reject)=> {
-const mock = mocks[location]
-
-if(!mock){
-  reject('not found')
-}
-
-resolve(mock)
-  })
- 
-};
-
-
-*/
-export const restaurantsTransform = ({ results = [] }) => {
-  const mappedResults = results.map((restaurant) => {
-    return {
-      ...restaurant,
-      address: restaurant.vicinity,
-      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
-      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
-    };
+    resolve(mock);
   });
-
-  return camelize(mappedResults);
 };
+
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+};
+restaurantsRequest()
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
